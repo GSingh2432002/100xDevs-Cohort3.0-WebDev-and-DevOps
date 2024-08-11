f3b9de17f71328f5be5f45db24d6bdfe9390ad04
GSingh2432002 <gsingh332211@gmail.com>
2024-08-11 16:38:42 +0530

    Update

--- Week1/Assignment5.js
+++ Week1/Assignment5.js
@@ -1,11 +1,5 @@
 // Write a function called sum that finds the sum from 1 to a number
 function sum(n){
-    let ans = 0;
-    for(let i=1; i<=n; i++){
-        ans = ans + i;
-    }
-    return ans;
+    return (n * (n + 1)) / 2;
 }
-
-const ans = sum(10);
-console.log(ans);
+console.log(sum(5));
