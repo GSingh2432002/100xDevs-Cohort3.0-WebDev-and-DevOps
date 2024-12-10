4209368dd0ddab908501de6ce3d8798bcd5d4a01
Gaurav Kumar Singh <gsingh332211@gmail.com>
2024-12-11 01:08:18 +0530

    Sidebar Completed with Readme Updated

---
+++ Week 13 - Tailwind/Day 02 - Creating Sidebars/Lecture Code/01_Transition and Sidebar/src/sidebar.jsx
@@ -0,0 +1,13 @@
+export function Sidebar() {
+    return(
+        <div className="flex">
+            <div className="transition-all ease-in-out duration-150 md:w-96 h-screen w-0">
+                Sidebar
+            </div>
+
+            <div className="bg-yellow-500 h-screen flex-1">
+                Content
+            </div>
+        </div>
+    )
+}
