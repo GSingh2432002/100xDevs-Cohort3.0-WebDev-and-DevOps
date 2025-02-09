239254e3f7d557dce0f8685e04ccf32b0d6247bd
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-02-09 23:56:18 +0530

    Excalidraw Project

--- Week 22 - Excalidraw/Day 01 - Excalidraw Part - I/Lecture Code/apps/http-backend/src/index.ts
+++ Week 22 - Excalidraw/Day 01 - Excalidraw Part - I/Lecture Code/apps/http-backend/src/index.ts
@@ -1,17 +1,36 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        userId: 123
-    })
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
+  res.json({
+    userId: 123,
+  });
 });
 
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign(
     {
@@ -21,13 +40,20 @@ app.post("/signin", (req, res) => {
   );
 
   res.json({
-    token
-  })
+    token,
+  });
 });
 
 app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        roomId: 123
-    })
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
+  res.json({
+    roomId: 123,
+  });
 });
 app.listen(4000);
